d0cf2e5217a524d49b119a34b1262600f00726bd
sophie-24 <gyuri5578@naver.com>
2026-05-17 03:10:14 +0900

    feat(#7): 내 고민 및 리액션 조회(GET /users/me/posts) API 구현

--- src/main/java/org/sopt/sopkathon/domain/repository/PostRepository.java
+++ src/main/java/org/sopt/sopkathon/domain/repository/PostRepository.java
@@ -3,7 +3,9 @@ package org.sopt.sopkathon.domain.repository;
 import org.sopt.sopkathon.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserIdAndIsDeletedFalse(Long userId);
 }
--- src/main/java/org/sopt/sopkathon/domain/repository/ReactionRepository.java
+++ src/main/java/org/sopt/sopkathon/domain/repository/ReactionRepository.java
@@ -4,6 +4,9 @@ import org.sopt.sopkathon.domain.entity.Reaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+    List<Reaction> findByPostId(Long postId);
 }
