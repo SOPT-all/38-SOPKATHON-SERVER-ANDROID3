e86e8123c428d5cb111fe33f572f29f45d086bbc
Jyunee54 <jyunee54@gmail.com>
2026-05-17 02:31:52 +0900

    feat(#4): User Entity 설정

--- src/main/java/org/sopt/sopkathon/domain/entity/User.java
+++ src/main/java/org/sopt/sopkathon/domain/entity/User.java
@@ -1,7 +1,11 @@
 package org.sopt.sopkathon.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Entity
+@Getter
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
