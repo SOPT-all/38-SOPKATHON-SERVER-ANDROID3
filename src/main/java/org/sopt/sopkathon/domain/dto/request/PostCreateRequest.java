208895ac3f6c45223ca3a9047cf39f43fc55102b
Jyunee54 <jyunee54@gmail.com>
2026-05-17 06:16:34 +0900

    chore: title 수정

--- src/main/java/org/sopt/sopkathon/domain/dto/request/PostCreateRequest.java
+++ src/main/java/org/sopt/sopkathon/domain/dto/request/PostCreateRequest.java
@@ -3,7 +3,7 @@ package org.sopt.sopkathon.domain.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record PostCreateRequest(
-        @NotBlank String title,
+        String title,
         @NotBlank String content
 ) {
 }
